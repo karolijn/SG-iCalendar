9fa5199119a71fd62162a2d309439805136bbe93
Your Name <your_email@your-domain.com>
2010-09-25 14:39:11 -0400

    added missing requires

--- SG_iCal.php
+++ SG_iCal.php
@@ -35,6 +35,9 @@ class SG_iCal {
 		require_once dirname(__FILE__) . '/helpers/SG_iCal_Line.php'; // BUILD: Remove line
 		require_once dirname(__FILE__) . '/helpers/SG_iCal_Query.php'; // BUILD: Remove line
 		require_once dirname(__FILE__) . '/helpers/SG_iCal_Parser.php'; // BUILD: Remove line
+                require_once dirname(__FILE__) . '/helpers/SG_iCal_Recurrence.php'; // BUILD: Remove line
+                require_once dirname(__FILE__) . '/helpers/SG_iCal_Freq.php'; // BUILD: Remove line
+                require_once dirname(__FILE__) . '/helpers/SG_iCal_Duration.php'; // BUILD: Remove line
 
 		if( $url !== false ) {
 			SG_iCal_Parser::Parse($url, $this);
